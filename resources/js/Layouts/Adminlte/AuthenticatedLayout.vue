7202def410c9c437295a11c00f3864fe1cbffc9a
Klaas Rikhotso <klaas@blackboardbs.com>
2024-05-05 18:08:31 +0200

    Add adminlte

---
+++ resources/js/Layouts/Adminlte/AuthenticatedLayout.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref } from 'vue';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+import NavLink from '@/Components/NavLink.vue';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import { Link } from '@inertiajs/vue3';
+import { Sidebar } from "@/Layouts/Adminlte";
+
+const showingNavigationDropdown = ref(false);
+</script>
+
+<template>
+    <div>
+        <div class="min-h-screen bg-gray-100">
+
+            <div class="wrapper">
+                <Sidebar></Sidebar>
+                <!-- Page Content -->
+
+                <!-- Content Wrapper. Contains page content -->
+                <div class="content-wrapper">
+                    <!-- Content Header (Page header) -->
+                    <div class="content-header bg-white shadow" v-if="$slots.header">
+                        <div class="container-fluid">
+                            <div class="row mb-2">
+                                <slot name="header" />
+                            </div><!-- /.row -->
+                        </div><!-- /.container-fluid -->
+                    </div>
+                    <!-- /.content-header -->
+
+                    <!-- Main content -->
+                    <div class="content">
+                        <div class="container-fluid mt-7">
+                            <main>
+                                <slot />
+                            </main>
+                        </div>
+                        <!-- /.container-fluid -->
+                    </div>
+                    <!-- /.content -->
+                </div>
+                <!-- /.content-wrapper -->
+
+                <!-- Main Footer -->
+                <footer class="main-footer">
+                    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+                    All rights reserved.
+                    <div class="float-right d-none d-sm-inline-block">
+                        <b>Version</b> 3.2.0
+                    </div>
+                </footer>
+            </div>
+            <!-- ./wrapper -->
+        </div>
+    </div>
+</template>
---
+++ resources/js/Layouts/Adminlte/index.js
@@ -0,0 +1,3 @@
+export { default as AuthenticatedLayout } from './AuthenticatedLayout.vue';
+export { default as Header } from './Header.vue';
+export { default as Sidebar } from './Sidebar.vue';
