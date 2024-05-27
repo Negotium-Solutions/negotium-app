39a72c5f2489333de66901f96b4469f1b81d365c
Jaco Ferreira <43100572+Cypher1983@users.noreply.github.com>
2024-05-27 22:58:17 +0200

    redirect to login

---
+++ resources/js/Pages/Dashboard/Index.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { AuthenticatedLayout } from '@/Layouts/Adminlte'
+import axios from 'axios';
+import {router, usePage} from '@inertiajs/vue3';
+import { computed } from "vue";
+import { useToastr } from "@/toastr.js";
+
+
+const toastr = useToastr();
+const page = usePage();
+const user = computed(() => page.props.auth.user);
+const negotium_api_url = computed(() => page.props.negotium_api_url);
+
+</script>
+
+<template>
+    <AuthenticatedLayout>
+        <template #header>
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6 text-right pt-2">
+          </div><!-- /.col -->
+        </template>
+
+        <div class="row">
+          </div>
+        <!-- /.row -->
+    </AuthenticatedLayout>
+</template>
+<style>
+  .btn-edit {
+    color: lawngreen;
+  }
+
+  .btn-delete {
+    color: orangered;
+  }
+</style>
