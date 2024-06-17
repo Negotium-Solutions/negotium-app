8429938e5d2841e4f179853b38e4335ebb297d4d
Jaco Ferreira <43100572+Cypher1983@users.noreply.github.com>
2024-06-17 23:58:34 +0200

    create step styling

--- resources/js/Pages/Step/Create.vue
+++ resources/js/Pages/Step/Create.vue
@@ -74,9 +74,9 @@ function editStep() {
           <li class="breadcrumb-item active">Processes</li>
         </ol>
       </div><!-- /.col -->
-      <div class="col-sm-6 text-right pt-3">
-        <a :href="route('process')" class="flex gap-2 justify-center py-2.5 px-3 text-xs leading-3 rounded border border-solid border-neutral-700 border-opacity-20 text-neutral-700 hover:bg-neutral-700 hover:text-white">Cancel</a>
-        <a :href="route('process.create')" class="px-4 py-2 bg-neutral-700 rounded border border-neutral-700 justify-center items-center text-white">Save Draft</a>
+      <div class="col-sm-6 text-right pt-2 pt-sm-3 pt-md-3 pt-lg-3 pt-xl-3">
+        <a :href="route('process')" class="px-4 py-2 rounded border border-neutral-700 justify-center items-center border border-solid border-neutral-700 border-opacity-20 text-neutral-700 hover:bg-neutral-700 hover:text-white">Cancel</a>
+        <a :href="route('process.create')" class="ml-2 px-4 py-2 bg-neutral-700 rounded border border-neutral-700 justify-center items-center text-white">Save Draft</a>
       </div><!-- /.col -->
     </template>
 
@@ -85,24 +85,30 @@ function editStep() {
       <div class="row">
         <div class="col-md-3 col-sm-12 pr-0">
           <div class="card card-default">
-            <div class="card-header">
-              <h3 class="card-title text-bold">Process Information</h3>
+            <div class="card-header border-bottom-0 pb-0">
+              <div class="text-neutral-700 text-[1.25rem] font-bold font-['Roboto'] leading-loose">Process Info</div>
             </div>
             <!-- /.card-header -->
             <!-- form start -->
             <form class="form-horizontal">
               <div class="card-body">
+                <div class="mb-1">
+                  <div class="opacity-50 text-neutral-700 text-xs font-normal font-['Nunito'] leading-3">Process name</div>
+                </div>
                 <div class="mb-2">
-                  <label for="step-name" class="font-weight-normal mb-0 gray"><small>Process name</small></label>
+                  <div class="text-neutral-700 text-sm font-medium font-['Roboto'] leading-tight">Onboarding for new interns</div>
                 </div>
                 <div class="mb-1">
-                  <label for="step-name" class="font-weight-normal mb-0 gray"><small>Category</small></label>
+                  <div class="opacity-50 text-neutral-700 text-xs font-normal font-['Nunito'] leading-3">Category</div>
+                </div>
+                <div class="mb-1 d-flex">
+                  <div class="text-neutral-700 text-sm font-medium font-['Roboto'] leading-tight">Category type 2</div> <div class="ml-1 mt-[0.15rem] w-3 h-3 bg-amber-500 rounded-[3px]"></div>
                 </div>
                 <div class="mt-3">
-                  <hr/>
+                  <div class="h-0.5 opacity-10 bg-neutral-700 rounded-[1px]"></div>
                 </div>
                 <div class="mt-3">
-                  <label for="steps" class="font-weight-normal mb-0 gray"><small>Process Steps</small></label>
+                  <div class="opacity-50 text-neutral-700 text-xs font-normal font-['Nunito'] leading-3">Process steps</div>
                 </div>
                 <div>
                   <button @click="selectStep(step)" v-for="(step, index) in useStepStore.steps" :key="index" type="button" class="btn btn-sm btn-outline-dark w-100 m-1"  :class="{ active : pageProps.selectedStep === step.id }">
@@ -120,7 +126,7 @@ function editStep() {
         <div class="col-md-4 col-sm-12 pl-0 pr-0">
           <div class="card card-default">
             <div class="card-header">
-              <h3 class="card-title text-bold">Step</h3><br/>
+              <div class="text-neutral-700 text-[1.25rem] font-bold font-['Roboto'] leading-loose">Step</div>
               <div v-if="useStepStore.step.name !== null" class="">
                 <label for="step-name" class="font-weight-normal mb-0"><small>Step name</small></label>
                 <div class="row" v-if="pageProps.isStepEdit === false">
@@ -150,9 +156,15 @@ function editStep() {
 
               </div>
               <!-- /.card-body -->
-              <div class="card-footer">
-                <button type="submit" class="btn btn-outline-light">Cancel</button>
-                <button type="submit" class="btn btn-default float-right">Add Activity</button>
+              <div class="card-footer text-right">
+                <!-- <button type="submit" class="btn btn-outline-light">Cancel</button> -->
+                <!-- <button type="submit" class="btn btn-default float-right">Add Activity</button> -->
+                <div class="w-28 h-[38px] p-3 bg-white rounded border border-neutral-700 justify-center items-center gap-2 inline-flex">
+                  <div class="text-emerald-400 text-xs font-medium font-['Roboto'] leading-[14px]">Add Activity</div>
+                </div>
+                <div class="ml-2 w-28 h-[38px] p-3 bg-white rounded border border-neutral-700 justify-center items-center gap-2 inline-flex">
+                  <div class="text-blue-400 text-xs font-medium font-['Roboto'] leading-[14px]">Add Title</div>
+              </div>
               </div>
             </form>
           </div>
@@ -162,9 +174,18 @@ function editStep() {
         </div>
         <div class="col-md-2 col-sm-12 md:mt-7 md:pt-12 pl-0">
           <div class="btn-group-vertical btn-block">
-            <button type="button" class="btn btn-md btn-outline-light btn-block">Add Standard Step</button>
-            <button type="button" class="btn btn-md btn-outline-light btn-block">Add decision step</button>
-            <button type="button" class="btn btn-md btn-outline-light btn-block">End process</button>
+            <div class="w-100 h-[38px] p-3 bg-white rounded-tl rounded-tr border border-neutral-700 items-center gap-2 inline-flex">
+                    <div class="text-emerald-700 text-xs font-medium font-['Roboto'] leading-[14px]">Add standard Step</div>
+            </div>
+            <div class="w-100 h-[38px] p-3 bg-white rounded-tl rounded-tr border border-neutral-700 items-center gap-2 inline-flex">
+                    <div class="text-sky-700 text-xs font-medium font-['Roboto'] leading-[14px]">Add decision Step</div>
+            </div>
+            <div class="w-100 h-[38px] p-3 bg-white rounded-tl rounded-tr border border-neutral-700 items-center gap-2 inline-flex">
+                    <div class="text-slate-700 text-xs font-medium font-['Roboto'] leading-[14px]">End process</div>
+            </div>
+            <!-- <button type="button" class="btn btn-md btn-outline-light btn-block">Add Standard Step</button> -->
+            <!-- <button type="button" class="btn btn-md btn-outline-light btn-block">Add decision step</button> -->
+            <!-- <button type="button" class="btn btn-md btn-outline-light btn-block">End process</button> -->
           </div>
         </div>
       </div>
