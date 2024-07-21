987ceb6870aac9f4b2f0cd23c26099ac7dbab8fb
Jaco Ferreira <jacob.j.ferreira1@gmail.com>
2024-07-21 16:59:26 +0200

    light stylesheet

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import '../css/app.css';
 import '../css/icons.css';
+import '../css/light.css';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
