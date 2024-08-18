debd399e7a32574fc4dc7561c6bcee0f12ac8619
Jaco Ferreira <jacob.j.ferreira1@gmail.com>
2024-08-18 21:06:26 +0200

    default page and add note modal styling

--- resources/js/Pages/Profile/Notes/Index.vue
+++ resources/js/Pages/Profile/Notes/Index.vue
@@ -70,7 +70,7 @@ onMounted(() => {
           </div>
         </div>
       </div>
-      <table v-else class="w-100">
+      <table v-else class="w-100 mt-4 pt-4">
         <tr>
           <td>
             <div class="text-center" v-html="no_notes_for_profile"></div>
@@ -82,64 +82,70 @@ onMounted(() => {
       </table>
     </div>
   </div>
-  <Dialog v-model:visible="show_add_note" modal header="Add note or reminder" :style="{ width: '40vw' }" :breakpoints="{ '1199px': '75vw', '575px': '90vw' } ">
-    <hr/>
-    <div class="mt-2">
-      <span>Note Subject</span>
+  <Dialog v-model:visible="show_add_note" modal header="Add note or reminder" :style="{ width: '40vw' }" :class="'notes-dialog'" :breakpoints="{ '1199px': '75vw', '575px': '90vw' } ">
+    <template #header>
+      <div class="row m-0 p-0 pb-2">
+        <div class="text-neutral-700 text-[1.5rem] font-bold font-['Roboto'] leading-loose w-100">Add note or reminder</div>
+      </div>
+    </template>
+    <div>
+      <span class="text-neutral-700 text-xs font-normal font-['Nunito'] leading-3">Note Subject</span>
       <div class="w-100">
-        <input type="text">
+        <input type="text" class="form-control">
       </div>
     </div>
     <div class="mt-2">
-      <span>Note</span>
-      <div class="border-1 rounded-sm">
-        <div class="mb-2 flex flex-wrap gap-2">
-          <button @click="formatText('bold')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white">
+      <span class="text-neutral-700 text-xs font-normal font-['Nunito'] leading-3">Note</span>
+      <div class="form-control-border rounded-sm bg-zinc-100">
+        <div class="flex flex-wrap gap-2">
+          <button @click="formatText('bold')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white text-sm">
             <i class="fas fa-bold"></i>
           </button>
-          <button @click="formatText('italic')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white">
+          <button @click="formatText('italic')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white text-sm">
             <i class="fas fa-italic"></i>
           </button>
-          <button @click="formatText('underline')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white">
+          <button @click="formatText('underline')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white text-sm">
             <i class="fas fa-underline"></i>
           </button>
-          <button @click="formatText('strikeThrough')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white">
+          <button @click="formatText('strikeThrough')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white text-sm">
             <i class="fas fa-strikethrough"></i>
           </button>
-          <button @click="formatText('justifyLeft')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white">
+          <button @click="formatText('justifyLeft')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white text-sm">
             <i class="fas fa-align-left"></i>
           </button>
-          <button @click="formatText('justifyCenter')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white">
+          <button @click="formatText('justifyCenter')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white text-sm">
             <i class="fas fa-align-center"></i>
           </button>
-          <button @click="formatText('justifyRight')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white">
+          <button @click="formatText('justifyRight')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white text-sm">
             <i class="fas fa-align-right"></i>
           </button>
-          <button @click="formatText('insertOrderedList')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white">
+          <button @click="formatText('insertOrderedList')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white text-sm">
             <i class="fas fa-list-ol"></i>
           </button>
-          <button @click="formatText('insertUnorderedList')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white">
+          <button @click="formatText('insertUnorderedList')" class="p-2 text-neutral-700 hover:bg-neutral-700 hover:text-white text-sm">
             <i class="fas fa-list-ul"></i>
           </button>
         </div>
-        <div class="border rounded-sm p-4 bg-white min-h-[200px]" contenteditable="true" ref="editor" @input="updateContent">
+        <div class="p-2 bg-white min-h-[200px] notes-editor" contenteditable="true" ref="editor" @input="updateContent">
           <p>Type your content here...</p>
         </div>
       </div>
     </div>
     <div class="mt-2">
-      <span>Set Reminder</span>
-      <div class="w-100">
-        <input type="datetime-local" class="col-sm-6  gap-2">
-        <input type="time" class="col-sm-6  pl-2">
+      <span class="text-neutral-700 text-xs font-normal font-['Nunito'] leading-3">Set Reminder</span>
+      <div class="d-flex gap-2 w-100">
+        <input type="date" class="form-control">
+        <input type="time" class="form-control">
       </div>
     </div>
-    <div class="d-flex w-100 pt-4 pl-2 pr-2 pb-3">
-      <div class="col-sm-6 text-right">
-        <button class="gap-2 justify-center py-2.5 px-3 text-sm leading-3 rounded-custom-25 border border-solid border-neutral-700 border-opacity-20 text-neutral-700 hover:bg-neutral-700 hover:text-white">Cancel</button>
-        <button class="gap-2 justify-center py-2.5 px-3 text-sm leading-3 rounded-custom-25 border border-solid border-neutral-700 border-opacity-20 text-neutral-700 hover:bg-neutral-700 hover:text-white">Add Note</button>
+    <template #footer>
+      <div class="row">
+        <div class="col-12 p-4 pr-0 text-right">
+          <button class="gap-2 justify-center py-2 px-4 rounded-custom-25 border border-solid border-neutral-700 border-opacity-20 text-neutral-700 hover:bg-neutral-700 hover:text-white mr-2" @click="cancel">Cancel</button>
+          <button class="gap-2 justify-center py-2 px-4 rounded-custom-25 border border-solid border-neutral-700 border-opacity-20 text-neutral-700 hover:bg-neutral-700 hover:text-white">Add Note</button>
+        </div>
       </div>
-    </div>
+    </template>
   </Dialog>
   </ExtendProfileLayout>
 </template>
