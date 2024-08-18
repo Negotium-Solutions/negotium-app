0ae02d6ab2d1a18591854a5f3028623870251e23
Jaco Ferreira <jacob.j.ferreira1@gmail.com>
2024-08-18 13:10:00 +0200

    personal details edit framework

--- resources/js/Pages/Profile/ProfileDetails/Edit.vue
+++ resources/js/Pages/Profile/ProfileDetails/Edit.vue
@@ -12,143 +12,144 @@ const profileManagerStore = useProfilesManagerStore();
         <h1 class="text-neutral-700 text-[1.5rem] font-bold font-['Roboto']">Profile Details</h1>
       </div>
       <div class="col-sm-6 text-right">
-        <button class="gap-2 justify-center py-2.5 px-3 text-sm leading-3 rounded-custom-25 border border-solid border-neutral-700 border-opacity-20 text-neutral-700 hover:bg-neutral-700 hover:text-white">Add Details</button>
+        <a class="gap-2 justify-center py-2 px-4 rounded-custom-25 border border-solid border-neutral-700 border-opacity-20 text-neutral-700 hover:bg-neutral-700 hover:text-white mr-2">Cancel</a>
+        <a class="gap-2 justify-center py-2 px-4 rounded-custom-25 border border-solid border-neutral-700 border-opacity-20 text-neutral-700 hover:bg-neutral-700 hover:text-white">Save</a>
       </div>
     </div>
-    <div class="d-flex w-100 pt-2 pl-2 pr-2 pb-1">
-      <h6 class="text-neutral-700 text-md mb-3 font-bold font-['Roboto']">Personal Information</h6>
-    </div>
-    <div class="d-flex w-100 pl-2">
+    <div class="d-flex flex-column w-100 pl-2 pr-2">
+      <div class="flex flex-col px-2 py-2 rounded-lg border border-solid border-neutral-700 mb-2">
+        <div class="d-flex w-100 pt-2 pl-2 pr-2 pb-1">
+          <h6 class="text-neutral-700 text-md mb-1 font-bold font-['Roboto']">Personal Information</h6>
+        </div>
+        <div class="d-flex w-100 pl-2">
 
-      <div class="flex-1 pr-10 border-left-2 border-primary">
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="first_name">First Name</label>
-          <input class="w-100 p-1 text-right" type="text" id="first_name" v-model="profileManagerStore.profile.first_name">
-        </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="first_name">Last Name</label>
-          <input class="w-100 p-1 text-right" type="text" id="first_name" v-model="profileManagerStore.profile.last_name">
-        </div>
-      </div>
-      <div class="bg-gray w-1"></div>
-      <div class="flex-1 pl-10 border-right-2">
-        <div class="d-flex align-items-center gap-28">
-          <label class="d-block mb-1" for="middle_name">Maiden Name</label>
-          <input class="w-100 p-1 mr-3 text-right" type="text" id="middle_name" >
-        </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="d-block mb-1" for="id_number">ID Number</label>
-          <input class="w-100 p-1 mr-3 text-right" type="text" id="id_number" v-model="profileManagerStore.profile.id">
-        </div>
-      </div>
-    </div>
-    <hr>
-    <div class="d-flex w-100 pt-2 pl-2 pr-2 pb-1">
-      <h6 class="text-neutral-700 text-md mb-3 font-bold font-['Roboto']">Contact Details</h6>
-    </div>
-    <div class="d-flex w-100 pl-2">
-
-      <div class="flex-1 pr-10 border-left-2 border-primary">
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="email">Email Address</label>
-          <input class="w-100 p-1 text-right" type="text" id="email" v-model="profileManagerStore.profile.email">
-        </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="mobile_number">Mobile Number</label>
-          <input class="w-100 p-1 text-right" type="text" id="mobile_number" >
+          <div class="flex-1 pr-7 border-top-0 border-bottom-0 border-left-0 border-right-2 border border-solid border-neutral-700 border-opacity-20">
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">First Name</span>
+              <input class="form-control" v-model="profileManagerStore.profile.first_name">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Last Name</span>
+              <input class="form-control" v-model="profileManagerStore.profile.last_name">
+            </div>
+          </div>
+          <div class="flex-1 pl-7 border-right-2">
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Maiden Name</span>
+              <input class="form-control" v-model="profileManagerStore.profile.maiden_name">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">ID Number</span>
+              <input class="form-control" v-model="profileManagerStore.profile.id">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="bg-gray w-1"></div>
-      <div class="flex-1 pl-10 border-right-2">
-        <div class="d-flex align-items-center gap-28">
-          <label class="d-block mb-1" for="work_number">Work Number</label>
-          <input class="w-100 p-1 mr-3 text-right" type="text" id="work_number" >
+      <div class="flex flex-col px-2 py-2 rounded-lg border border-solid border-neutral-700 mb-2">
+        <div class="d-flex w-100 pt-2 pl-2 pr-2 pb-1">
+          <h6 class="text-neutral-700 text-md mb-1 font-bold font-['Roboto']">Contact Details</h6>
         </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="d-block mb-1" for="home_number">Home Number</label>
-          <input class="w-100 p-1 mr-3 text-right" type="text" id="home_number">
-        </div>
-      </div>
-    </div>
-    <hr/>
-    <div class="d-flex w-100 pt-2 pl-2 pr-2 pb-1">
-      <h6 class="text-neutral-700 text-md mb-3 font-bold font-['Roboto']">Home Address</h6>
-    </div>
-    <div class="d-flex w-100 pl-2">
-      <div class="flex-1 pr-10 border-left-2 border-primary">
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="residence_type">Residence type</label>
-          <input class="w-100 p-1 text-right" type="text" id="residence_type">
-        </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="building_name">Building Name</label>
-          <input class="w-100 p-1 text-right" type="text" id="building_name" >
-        </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="unit_number">Unit Number</label>
-          <input class="w-100 p-1 text-right" type="text" id="unit_number">
-        </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="street_address">Street Name</label>
-          <input class="w-100 p-1 text-right" type="text" id="street_address" >
+        <div class="d-flex w-100 pl-2">
+          <div class="flex-1 pr-7 border-top-0 border-bottom-0 border-left-0 border-right-2 border border-solid border-neutral-700 border-opacity-20">
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Email Address</span>
+              <input class="form-control" v-model="profileManagerStore.profile.email">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Mobile Number</span>
+              <input class="form-control" v-model="profileManagerStore.profile.cell">
+            </div>
+          </div>
+          <div class="flex-1 pl-10 border-right-2">
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Work Number</span>
+              <input class="form-control" v-model="profileManagerStore.profile.work">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Home Number</span>
+              <input class="form-control" v-model="profileManagerStore.profile.home">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="bg-gray w-1"></div>
-      <div class="flex-1 pl-10 border-right-2">
-        <div class="d-flex align-items-center gap-20">
-          <label class="mb-1" for="suburb">Suburb</label>
-          <input class="w-100 p-1 mr-3 text-right" type="text" id="suburb" >
+      <div class="flex flex-col px-2 py-2 rounded-lg border border-solid border-neutral-700 mb-2">
+        <div class="d-flex w-100 pt-2 pl-2 pr-2 pb-1">
+          <h6 class="text-neutral-700 text-md mb-1 font-bold font-['Roboto']">Home Address</h6>
         </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="city">City</label>
-          <input class="w-100 p-1 mr-3 text-right" type="text" id="city">
-        </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="country">Country</label>
-          <input class="w-100 p-1 text-right" type="text" id="country">
-        </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="postal_code">Postal Code</label>
-          <input class="w-100 p-1 text-right" type="text" id="postal_code" >
-        </div>
-      </div>
-    </div>
-    <hr/>
-    <div class="d-flex w-100 pt-2 pl-2 pr-2 pb-1">
-      <h6 class="text-neutral-700 text-md mb-3 font-bold font-['Roboto']">Work Address</h6>
-    </div>
-    <div class="d-flex w-100 pl-2">
-
-      <div class="flex-1 pr-10 border-left-2 border-primary">
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="building_name">Building Name</label>
-          <input class="w-100 p-1 text-right" type="text" id="building_name" >
-        </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="work_street_address">Street Address</label>
-          <input class="w-100 p-1 text-right" type="text" id="work_street_address">
-        </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="work_suburb">Suburb</label>
-          <input class="w-100 p-1 text-right" type="text" id="work_suburb" >
-        </div>
-      </div>
-      <div class="bg-gray w-1"></div>
-      <div class="flex-1 pl-10 border-right-2">
-        <div class="d-flex align-items-center gap-28">
-          <label class="d-block mb-1" for="work_city">City</label>
-          <input class="w-100 p-1 mr-3 text-right" type="text" id="work_city">
-        </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="work_country">Country</label>
-          <input class="w-100 p-1 text-right" type="text" id="work_country">
-        </div>
-        <div class="d-flex align-items-center gap-28">
-          <label class="mb-1" for="work_postal_code">Postal Code</label>
-          <input class="w-100 p-1 text-right" type="text" id="work_postal_code" >
+        <div class="d-flex w-100 pl-2">
+          <div class="flex-1 pr-7 border-top-0 border-bottom-0 border-left-0 border-right-2 border border-solid border-neutral-700 border-opacity-20">
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Residence type</span>
+                <input class="form-control">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Building Name</span>
+              <input class="form-control">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Unit Number</span>
+              <input class="form-control">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Street Name</span>
+              <input class="form-control">
+            </div>
+          </div>
+          <div class="flex-1 pl-10 border-right-2">
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Suburb</span>
+              <input class="form-control">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">City</span>
+              <input class="form-control">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Country</span>
+              <input class="form-control">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Postal Code</span>
+              <input class="form-control">
+            </div>
+          </div>
         </div>
       </div>
-      <hr/>
+      <div class="flex flex-col px-2 py-2 rounded-lg border border-solid border-neutral-700 mb-2">
+        <div class="d-flex w-100 pt-2 pl-2 pr-2 pb-1">
+          <h6 class="text-neutral-700 text-md mb-1 font-bold font-['Roboto']">Work Address</h6>
+        </div>
+        <div class="d-flex w-100 pl-2">
+          <div class="flex-1 pr-7 border-top-0 border-bottom-0 border-left-0 border-right-2 border border-solid border-neutral-700 border-opacity-20">
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Building Name</span>
+              <input class="form-control">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Street Address</span>
+              <input class="form-control">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Suburb</span>
+              <input class="form-control">
+            </div>
+          </div>
+          <div class="flex-1 pl-10 border-right-2">
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">City</span>
+              <input class="form-control">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Country</span>
+              <input class="form-control">
+            </div>
+            <div class="gap-28 mb-1">
+              <span class="mb-1 font-normal text-sm">Postal Code</span>
+              <input class="form-control">
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
